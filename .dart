d14ec4be1a8790e2d80f8b0ac7717f53bcb133e8
mauricio2093 <mauriciocadena2093@gmail.com>
2023-06-05 18:18:06 -0500

    01-Dart

---
+++ .dart
@@ -0,0 +1,5 @@
+const double price = 20.0;
+final int age = 10;
+void main() {
+  print(double.parse(price.toString()).round() + age);
+}
---
+++ variables.dart
@@ -0,0 +1,43 @@
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyOnjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+
+// aunque la variable almacene un tipo de referencia como string, puede cambiar ese tipo especificandolo
+// si un objeto no esta restringido aun solo tipo, se especifica Object como tipo o dinamic si es necesario
+
+Object name1 = 'Bob';
+
+dynamic name2 = 'Bob';
+
+// pero tambia se puede declarar explicitamente el tipo que se inferirá
+String name3 = 'Bob';
+
+// Otras variables
+var Name = '';
+final int age = 10; // final variable que solo se puede establecer una vez
+const double price = 20.0; // const constante en un tiempo de compilación
+late String aux; // late despues le va asignar un valor
+bool isOpen = false;
+List numbersList = [1, 2, 3, 4];
+
+void main() {
+  print(
+      'the satellite ${name} was created in ${year}, its antenna has a diameter of ${antennaDiameter}, He traveled through several planets as: ${flybyOnjects}');
+
+  print('${name1.runtimeType} ${name2.runtimeType} ${name3.runtimeType}');
+
+  print(double.parse(price.toString()).round() + age);
+}
+
+//Null Safety - Dart no permite variables de valor null
+
+String? animal; // Nullable type. Can be `null` or string.
+late String animal1; // Non-nullable type. Cannot be `null` but can be string.
+
+
+
