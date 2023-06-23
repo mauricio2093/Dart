4ebcc9092c5afc4eba0a7890e9fb9351a045bde7
mauricio2093 <mauriciocadena2093@gmail.com>
2023-06-22 21:14:38 -0500

    04-Dart

--- function.dart
+++ function.dart
@@ -1,1 +1,22 @@
+void main() {
+  myFuntion();
 
+  var result = fibonacci(20);
+  print(result);
+
+  var myRetorn = myFunctionWithReturn();
+  print(myRetorn);
+}
+
+void myFuntion() {
+  print("Esto es una funcion");
+}
+
+String myFunctionWithReturn() {
+  return "Esto es una funcion";
+}
+
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
