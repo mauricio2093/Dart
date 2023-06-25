7d4d26f85654e00c1dcd3fa8a6fe109801760a81
mauricio2093 <mauriciocadena2093@gmail.com>
2023-06-25 00:49:34 -0500

    09-Dart

--- tipoDeVariables.dart
+++ tipoDeVariables.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 void main() {
   var name = 'Voyager I';
   var year = 1977;
@@ -119,7 +121,7 @@ void main() {
   final String pokemon = 'Ditto';
   final int hp = 100;
   final bool isAlive = true;
-  final List abilities = <String>['impostor'];
+  final List<String> abilities = <String>['impostor'];
 
   //String Multiline
   print("""
@@ -128,4 +130,56 @@ void main() {
   $isAlive
   $abilities
   """);
+
+  // Dynamic == null
+  dynamic errorMessage = 'Hola';
+
+  for (int j = 0; j < 6; j++) {
+    switch (j) {
+      case 0:
+        print("esto es $errorMessage");
+      case 1:
+        print("esto es ${errorMessage = true}");
+      case 2:
+        print("esto es ${errorMessage = [1, 2, 3, 4, 5]}");
+      case 3:
+        print("esto es ${errorMessage = () => true}");
+      case 4:
+        print("esto es ${errorMessage = 1}");
+      case 5:
+        print("esto es ${errorMessage = null}");
+      case 6:
+        print(
+            "esto es ${errorMessage += 1}"); /* no genera error cuidado
+                                                 con dynamic */
+      default:
+        print("Error");
+    }
+  }
+  // Maps
+  final Map<String, dynamic> pokemons = {
+    'name': 'Ditto',
+    'HP': 100,
+    'isAlive': true,
+    'abilities': <String>['impostor'],
+    'sprites': <int, String>{
+      1: 'ditt/front.png',
+      2: 'ditto/backend.png',
+    }
+  };
+
+  print("Front ${pokemons['sprites'][1]}");
+  print("Backend ${pokemons['sprites'][2]}");
+
+  final pokemons2 = Map<String, dynamic>();
+  pokemons2["name"] = 'Ditto';
+  pokemons2['HP'] = 100;
+  pokemons2['isAlive'] = true;
+  pokemons2['abilities'] = <String>['impostor'];
+  pokemons2['sprites'] = Map<int, String>();
+  pokemons2['sprites'][1] = 'ditt/front.png';
+  pokemons2['sprites'][2] = 'ditt/backend.png';
+
+  print("Front ${pokemons2['sprites'][1]}");
+  print("Backend ${pokemons2['sprites'][2]}");
 }
