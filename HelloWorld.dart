a214d40c1cbf8ff261f41523d5310f8cb8838f11
mauricio2093 <mauriciocadena2093@gmail.com>
2023-06-24 23:25:56 -0500

    07-Dart

--- HelloWorld.dart
+++ HelloWorld.dart
@@ -1,3 +1,13 @@
 void main() {
-  print('Hello, World!');
+  const myHi = 'Hello';
+  String myWorld = 'World!';
+
+  for (int i = 0; i < 5; i++) {
+    bool isPair = i % 2 == 0;
+    if (isPair) {
+      print('$myHi, $myWorld ${i + 1}');
+    } else {
+      print('${myHi.toUpperCase()}, ${myWorld.toUpperCase()} ${i + 1}');
+    }
+  }
 }
