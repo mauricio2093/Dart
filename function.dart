c5e237a1baba5fa6ec41a7fcef4897e387f37ffc
mauricio2093 <mauriciocadena2093@gmail.com>
2023-06-25 22:46:17 -0500

    11-Dart

--- function.dart
+++ function.dart
@@ -6,6 +6,11 @@ void main() {
 
   var myRetorn = myFunctionWithReturn();
   print(myRetorn);
+
+  print(greetEveryone());
+
+  print('sum 2 numbers ${addTwoNumbers(73)}');
+  print('sum 3 numbers ${addThreeNumbers(73, 9)}');
 }
 
 void myFuntion() {
@@ -20,3 +25,13 @@ int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+String greetEveryone() => 'Hello everyone!';
+
+int addTwoNumbers(int a, [int? b]) {
+  b = b ?? 0; // condicionales null
+
+  return a + b;
+}
+
+int addThreeNumbers(int a, [int b = 0]) => a + b + 23;
