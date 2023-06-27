d815528536fda8fd74752bbeed14b54c0eac1438
mauricio2093 <mauriciocadena2093@gmail.com>
2023-06-26 22:35:11 -0500

    12-Dart

--- Class.dart
+++ Class.dart
@@ -1,3 +1,30 @@
+void main() {
+  final Hero wolverine = Hero('Logan', 'Regeneración');
+
+  print(wolverine);
+  print(wolverine.name);
+  print(wolverine.power);
+
+  final Animal felino = Animal(
+    name: 'Leon',
+    type: 'carnivoro',
+    isExtinct: false,
+  );
+
+  print(felino);
+  print(felino.name);
+  print(felino.type);
+
+  final Map<String, dynamic> rawJson = {
+    'brand': 'kia picanto',
+    'year': 2023,
+    'isUse': false,
+  };
+
+  final automobile = Car.fromMap(rawJson);
+  print(automobile.infoCar());
+}
+
 class MyClass {
   String name;
   int age;
@@ -6,4 +33,51 @@ class MyClass {
     this.name,
     this.age,
   );
+}
+
+class Hero {
+  String name;
+  String power;
+
+  Hero(String pName, String pPower)
+      : name = pName,
+        power = pPower;
+}
+
+class Animal {
+  String name;
+  String type;
+  bool isExtinct;
+
+  Animal({
+    required this.name,
+    this.type = 'Sin tipo',
+    required this.isExtinct,
+  });
+
+  @override //sobre escribiendo una funcion nativa
+  String toString() {
+    return '$name, $type, is Extinct: ${isExtinct ? 'YES!' : 'Nope'}';
+  }
+}
+
+class Car {
+  String brand;
+  int year;
+  bool isUse;
+
+  Car({
+    required this.brand,
+    required this.year,
+    required this.isUse,
+  });
+
+  Car.fromMap(Map<String, dynamic> json)
+      : brand = json['brand'] ?? 'No name found',
+        year = json['year'] ?? 'No year found',
+        isUse = json['isUse'] ?? 'No is Used found';
+
+  String infoCar() {
+    return '$brand, $year, was Used: ${isUse ? 'YES!' : 'Nope'}';
+  }
 }
